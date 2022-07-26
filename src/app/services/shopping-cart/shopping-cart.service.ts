8d1b19edf74f4e3f73446c47b8422e7fcb133d02
Georgina Adzorgenu <gadzorgenu@gmail.com>
2022-07-26 22:29:14 +0000

    Added shopping cart info

--- src/app/services/shopping-cart/shopping-cart.service.ts
+++ src/app/services/shopping-cart/shopping-cart.service.ts
@@ -13,37 +13,41 @@ export class ShoppingCartService {
 
   constructor(private http: HttpClient) { }
 
-  private createcart(): Observable<ShoppingCart> {
-    console.log(new Date().getDate)
-    return this.http.post<ShoppingCart>(`${this.base_url}/add`, { createdAt: new Date().getDate })
-  }
+  // private createcart(): Observable<ShoppingCart> {
+  //   console.log(new Date().getDate)
+  //   return this.http.post<ShoppingCart>(`${this.base_url}/add`, { createdAt: new Date().getDate })
+  // }
 
-  private getCart(cartId: string | null) {
-    return this.http.get<ShoppingCart>(`${this.base_url}/${cartId}`)
-  }
-  private getOrCreateCartId() {
-    let cartId = localStorage.getItem('cartId');
-    if (cartId) return cartId;
+  // private getCart(cartId: string | null) {
+  //   return this.http.get<ShoppingCart>(`${this.base_url}/${cartId}`)
+  // }
+  // private getOrCreateCartId() {
+  //   let cartId = localStorage.getItem('cartId');
+  //   if (cartId) return cartId;
 
-    this.createcart().subscribe(res => {
-      localStorage.setItem('cartId', res.id);
-      cartId = res.id
-    })
-    return cartId;
-  }
+  //   this.createcart().subscribe(res => {
+  //     localStorage.setItem('cartId', res.id);
+  //     cartId = res.id
+  //   })
+  //   return cartId;
+  // }
 
-  async addToCart(product: Product) {
-    let cartId =  this.getOrCreateCartId();
+  // async addToCart(product: Product) {
+  //   let cartId =  this.getOrCreateCartId();
 
-    console.log(cartId);
+  //   console.log(cartId);
 
-    let item$ = this.getProductInCart(cartId, product.id).subscribe(res => {
+  //   let item$ = this.getProductInCart(cartId, product.id).subscribe(res => {
 
-    })
+  //   })
 
-  }
+  // }
 
-  private getProductInCart(cartId: string | null, productId: string) {
-    return this.http.get<ShoppingCart>(`${this.base_url}/${cartId}/items/${productId}`)
+  // private getProductInCart(cartId: string | null, productId: string) {
+  //   return this.http.get<ShoppingCart>(`${this.base_url}/${cartId}/items/${productId}`)
+  // }
+
+  addToCart(productId: string, operation: string){
+    return this.http.post<ShoppingCart>(`${this.base_url}/${operation}`, productId);
   }
 }
